ada80a2bb6b6baaee7f14274d9f7673b8ec1c93f
Greg Lindahl <lindahl@pbm.com>
2021-05-25 18:44:24 +0000

    initial, working config

---
+++ nginx.conf.d
@@ -0,0 +1,34 @@
+server {
+    listen 64.13.139.229:443 ssl; # managed by Certbot
+    server_name  challenge.bx9.net;
+
+    access_log  /var/log/nginx/challenge.bx9.net.log  main;
+    add_header Permissions-Policy interest-cohort=() always;
+
+    location / {
+        root   /home/astrogreg/github/ngeht-analysis-content/output/;
+        index  index.html;
+    }
+
+    ssl_certificate /etc/letsencrypt/live/challenge.bx9.net/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/challenge.bx9.net/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+
+server {
+    listen       64.13.139.229:80;
+    server_name  challenge.bx9.net
+
+    access_log /var/log/nginx/lindahl_redir.log main;
+    add_header Permissions-Policy interest-cohort=() always;
+
+    location /.well-known/ {
+        root   /home/astrogreg/github/ngeht-analysis-content/output/.well-known/;
+        index  index.html;
+    }
+
+    location / {
+        rewrite ^/(.*)$ https://$host/$1 redirect;
+    }
+}
